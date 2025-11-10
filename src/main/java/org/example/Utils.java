9f2a07005aa0b26be342de8d1343f9cb506d01da
Victoria Grama <anastasiagrama@hostname.mshome.net>
2025-11-10 12:03:54 +0200

    first commit

---
+++ src/main/java/org/example/Utils.java
@@ -0,0 +1,32 @@
+package org.example.utils;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+import java.time.Duration;
+
+public class Utils {
+
+    WebDriver driver;
+
+    public Utils(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void scrollToElement(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    public static WebElement fluentWait(WebDriver driver, By by) {
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoSuchElementException.class);
+        return wait.until(dr -> driver.findElement(by));
+    }
+
+    public static <T> T explicitWait(WebDriver driver, ExpectedCondition<T> condition, long seconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        return wait.until(condition);
+    }
+}
