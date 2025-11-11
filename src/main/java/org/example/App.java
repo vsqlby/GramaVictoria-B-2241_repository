daa72c0e035761dab6ef47772526c2585e6b77be
Victoria Grama <anastasiagrama@hostname.mshome.net>
2025-11-11 09:13:55 +0200

    Selenium Test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,10 +9,8 @@ import org.openqa.selenium.Keys;
 import atu.testrecorder.ATUTestRecorder;
 import atu.testrecorder.exceptions.ATUTestRecorderException;
 import java.io.File;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class App {
 
@@ -22,86 +20,71 @@ public class App {
         WebDriver driver = null;
 
         try {
-            // Create folder for videos if not exists
+            // Create folder for videos
             File videoDir = new File("test-videos");
             if (!videoDir.exists()) videoDir.mkdir();
 
-            // Start recording
-            recorder = new ATUTestRecorder("test-videos", "FormTest_Video", false);
+            // Generate timestamp for unique filename
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            recorder = new ATUTestRecorder("test-videos", "FormTest_" + timestamp, false);
             recorder.start();
 
             // Setup driver
             WebDriverManager.chromedriver().setup();
-
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("--no-sandbox", "--disable-dev-shm-usage"); // headless removed
+            options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
             driver = new ChromeDriver(options);
             driver.manage().window().maximize();
 
             driver.get("https://demoqa.com/automation-practice-form");
 
-
-            // Hide ads and banners
             JavascriptExecutor js = (JavascriptExecutor) driver;
-            js.executeScript(
-                    "document.querySelectorAll('iframe, #fixedban, .Advertisement').forEach(el => el.style.display='none');"
-            );
+            js.executeScript("document.querySelectorAll('iframe, #fixedban, .Advertisement').forEach(el => el.style.display='none');");
 
-            // Fill in the form
             driver.findElement(By.id("firstName")).sendKeys("Grama");
             driver.findElement(By.id("lastName")).sendKeys("Victoria");
             driver.findElement(By.id("userEmail")).sendKeys("diva.queen@example.com");
             driver.findElement(By.xpath("//label[text()='Female']")).click();
             driver.findElement(By.id("userNumber")).sendKeys("1234567890");
 
-            // Date of Birth: scroll + JS click + wait
             WebElement dateInput = Utils.explicitWait(driver,
-                    ExpectedConditions.elementToBeClickable(By.id("dateOfBirthInput")), 10);
+                    org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable(By.id("dateOfBirthInput")), 10);
             Utils.scrollToElementStatic(driver, dateInput);
-            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", dateInput);
+            js.executeScript("arguments[0].click();", dateInput);
 
-            // Select month, year, day
             WebElement monthSelect = Utils.explicitWait(driver,
-                    ExpectedConditions.elementToBeClickable(By.className("react-datepicker__month-select")), 10);
+                    org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable(By.className("react-datepicker__month-select")), 10);
             monthSelect.sendKeys("May");
 
             WebElement yearSelect = Utils.explicitWait(driver,
-                    ExpectedConditions.elementToBeClickable(By.className("react-datepicker__year-select")), 10);
+                    org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable(By.className("react-datepicker__year-select")), 10);
             yearSelect.sendKeys("2000");
 
             WebElement daySelect = Utils.explicitWait(driver,
-                    ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class, 'react-datepicker__day') and text()='15']")), 10);
+                    org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class, 'react-datepicker__day') and text()='15']")), 10);
             daySelect.click();
 
-            // Subjects
             WebElement subjectInput = driver.findElement(By.id("subjectsInput"));
             subjectInput.sendKeys("Maths");
             subjectInput.sendKeys(Keys.ENTER);
 
-            // Hobbies
             driver.findElement(By.xpath("//label[text()='Sports']")).click();
             driver.findElement(By.xpath("//label[text()='Reading']")).click();
 
-            // Upload picture
             WebElement upload = driver.findElement(By.id("uploadPicture"));
             upload.sendKeys("/Users/anastasiagrama/Downloads/12499.jpg");
 
-            // Current address
             driver.findElement(By.id("currentAddress")).sendKeys("123 Main Street, New York");
 
-            // State and City
             WebElement state = driver.findElement(By.id("react-select-3-input"));
             state.sendKeys("NCR");
             state.sendKeys(Keys.ENTER);
-
             WebElement city = driver.findElement(By.id("react-select-4-input"));
             city.sendKeys("Delhi");
             city.sendKeys(Keys.ENTER);
-
-            // Scroll and submit
             WebElement submitButton = driver.findElement(By.id("submit"));
             Utils.scrollToElementStatic(driver, submitButton);
-            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", submitButton);
+            js.executeScript("arguments[0].click();", submitButton);
 
             Thread.sleep(3000);
 
